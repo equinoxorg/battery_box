40273f9381ca4c66daa50383e92a4f68a6bb004c
Ashley Grealish <ashley.grealish09@imperial.ac.uk>
2012-11-14 11:43:21 +0000

    Add license information

--- Software/LVCO.X/main.c
+++ Software/LVCO.X/main.c
@@ -1,8 +1,24 @@
+
 /*--------------------------- Low Voltage Cut Off ------------------------------
  | 	E.quinox battery box low voltage cutout circuit
- |      
- |      IDE : MPLAB X IDE v1.20
- |      Compiler: hi-tech-picc v9.83 - lite version
+ |  Copyright (C) 2011  Ashley Grealish - e.quinox.org
+ |
+ |  This program is free software: you can redistribute it and/or modify
+ |  it under the terms of the GNU General Public License as published by
+ |  the Free Software Foundation, either version 3 of the License, or
+ |  (at your option) any later version.
+ |
+ |  This program is distributed in the hope that it will be useful,
+ |  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ |  GNU General Public License for more details.
+ |
+ |  You should have received a copy of the GNU General Public License
+ |  along with this program.  If not, see <http://www.gnu.org/licenses/>.    
+ |
+ |
+ |  IDE : MPLAB X IDE v1.20
+ |  Compiler: hi-tech-picc v9.83 - lite version
  |
  |                   __________
  |	VCC         |1      14| GND
